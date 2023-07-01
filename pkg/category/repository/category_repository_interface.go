7a45a9780d06be7be82be9c3c551581da958da45
armin <genius2001armin@gmail.com>
2023-07-02 01:45:01 +0330

    repository:create category interface

---
+++ pkg/category/repository/category_repository_interface.go
@@ -0,0 +1,13 @@
+package repository
+
+import (
+	cm "restful_blog/pkg/category/model"
+)
+
+type CategoryRepositoryInterface interface {
+	AllCategories(page, perPage int) ([]*cm.Category, error)
+	CategoryByID(categoryId int64) (*cm.Category, error)
+	CreateCategory(category *cm.Category) error
+	UpdateCategory(category *cm.Category) error
+	DeleteCategory(categoryId int64) error
+}
