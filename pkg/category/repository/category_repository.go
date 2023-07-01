d25449c688de091fb29b90de5d353ae9e340b98a
armin <genius2001armin@gmail.com>
2023-07-02 01:44:15 +0330

    repository:create category repository

---
+++ pkg/category/repository/category_repository.go
@@ -0,0 +1,60 @@
+package repository
+
+import (
+	"gorm.io/gorm"
+
+	"restful_blog/pkg/category/model"
+)
+
+type CategoryRepositoryImpl struct {
+	db *gorm.DB
+}
+
+func NewCategoryRepository(db *gorm.DB) *CategoryRepositoryImpl {
+	return &CategoryRepositoryImpl{
+		db: db,
+	}
+}
+
+func (c *CategoryRepositoryImpl) CategoryByID(id int64) (*model.Category, error) {
+	var category model.Category
+	if err := c.db.First(&category, "id = ?", id).Error; err != nil {
+		return nil, err
+	}
+	return &category, nil
+}
+
+func (c *CategoryRepositoryImpl) AllCategories(page, perPage int) ([]*model.Category, error) {
+	var categories []*model.Category
+	offset := (page - 1) * perPage
+	err := c.db.Offset(offset).Limit(perPage).Model(categories).Find(&categories).Error
+
+	if err != nil {
+		return nil, err
+	}
+
+	return categories, nil
+}
+
+func (c *CategoryRepositoryImpl) CreateCategory(category *model.Category) error {
+	if err := c.db.Create(category).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *CategoryRepositoryImpl) UpdateCategory(category *model.Category) error {
+	if err := c.db.Save(category).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *CategoryRepositoryImpl) DeleteCategory(categoryId int64) error {
+	category := model.Category{ID: categoryId}
+
+	if err := c.db.Delete(category).Error; err != nil {
+		return err
+	}
+	return nil
+}
