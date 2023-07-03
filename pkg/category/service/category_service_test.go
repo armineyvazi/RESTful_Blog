7bd23e8af8c95af0684f3e3db9c048930af5d93f
armin <genius2001armin@gmail.com>
2023-07-03 16:59:42 +0330

    service_test:write unit test

--- pkg/category/service/category_service_test.go
+++ pkg/category/service/category_service_test.go
@@ -1,134 +1,173 @@
 package service
 
-// Test_Category_Service_Get_Category_By_Id test category service GetCategoryById method
-//func Test_Category_Service_Get_Category_By_Id(t *testing.T) {
-//	// Create a mock repository
-//	mockRepo := &repositories.MockCategoryRepository{}
-//
-//	// Set up the expected response from the mock repository
-//	expectedCategory := &model.Category{
-//		ID:   1,
-//		Name: "Category 1",
-//	}
-//
-//	// Mock category repository
-//	mockRepo.GetCategoryByIDFn = func(categoryID int64) (*model.Category, error) {
-//		return expectedCategory, nil
-//	}
-//
-//	// Create a new CategoryService instance with the mock  category repository
-//	s := &CategoryService{
-//		CategoryRepository: mockRepo,
-//	}
-//
-//	// Call the method
-//	category, err := s.GetCategoryByID(expectedCategory.ID)
-//
-//	// Assertions
-//	assert.NoError(t, err)
-//	assert.Equal(t, expectedCategory, category)
-//}
-//
-//// Test_Category_Service_Get_All_Category test category service GetAllCategory
-//func Test_Category_Service_Get_All_Category(t *testing.T) {
-//	// Create a mock repository
-//	mockRepo := &repositories.MockCategoryRepository{}
-//
-//	// Set up the expected response from the mock category repository
-//	expectedCategories := []*model.Category{
-//		{ID: 1, Name: "Category 1"},
-//		{ID: 2, Name: "Category 2"},
-//	}
-//	mockRepo.GetAllCategoriesFn = func(page, perPage int) ([]*model.Category, error) {
-//		return expectedCategories, nil
-//	}
-//
-//	// Create a new CategoryService instance with the mock category repository
-//	s := &CategoryService{
-//		CategoryRepository: mockRepo,
-//	}
-//
-//	// Call the method
-//	categories, err := s.GetAllCategory("1", "10")
-//
-//	// Assertions
-//	assert.NoError(t, err)
-//	assert.Equal(t, expectedCategories, categories)
-//}
-//
-//// Test_Category_Service_Create_Category test  category service CreateCategory service
-//func Test_Category_Service_Create_Category(t *testing.T) {
-//	// Create a mock category repository
-//	mockRepo := &repositories.MockCategoryRepository{}
-//
-//	// Set up the expected response from the mock category repository
-//	mockRepo.CreateCategoryFn = func(category *model.Category) error {
-//		category.ID = 1 // Assign the expected ID
-//		return nil
-//	}
-//
-//	// Create a new CategoryService instance with the mock category repository
-//	s := &CategoryService{
-//		CategoryRepository: mockRepo,
-//	}
-//
-//	// Create a new category
-//	category := &model.Category{
-//		Name: "New Category",
-//	}
-//
-//	// Call the method
-//	err := s.CreateCategory(category)
-//
-//	// Assertions
-//	assert.NoError(t, err)
-//	assert.Equal(t, int64(1), category.ID)
-//}
-//
-//// Test_Category_Service_Update_Category test category service UpdateCategory
-//func Test_Category_Service_Update_Category(t *testing.T) {
-//	// Create a mock repository
-//	mockRepo := &repositories.MockCategoryRepository{}
-//
-//	// Set up the expected response from the mock category repository
-//	mockRepo.UpdateCategoryFn = func(category *model.Category) error {
-//		return nil
-//	}
-//	// Create a new CategoryService instance with the mock category repository
-//	s := &CategoryService{
-//		CategoryRepository: mockRepo,
-//	}
-//	// Create a category to update
-//	category := &model.Category{
-//		ID:   1,
-//		Name: "Updated Category",
-//	}
-//
-//	// Call the method
-//	err := s.UpdateCategory(category)
-//
-//	// Assertions
-//	assert.NoError(t, err)
-//}
-//
-//// Test_Category_Service_Delete_Category test category service Delete method
-//func Test_Category_Service_Delete_Category(t *testing.T) {
-//	// Create a mock repository
-//	mockRepo := &repositories.MockCategoryRepository{}
-//
-//	// Set up the expected response from the mock category repository
-//	mockRepo.DeleteCategoryFn = func(categoryID int64) error {
-//		return nil
-//	}
-//
-//	// Create a new CategoryService instance with the mock category repository
-//	s := &CategoryService{
-//		CategoryRepository: mockRepo,
-//	}
-//
-//	// Call the method
-//	err := s.DeleteCategory(1)
-//
-//	// Assertions
-//	assert.NoError(t, err)
-//}
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+
+	"restful_blog/pkg/category/model"
+)
+
+type MockCategoryRepository struct {
+	mock.Mock
+}
+
+// Mock CreateCategory
+func (m *MockCategoryRepository) CreateCategory(category *model.Category) (*model.Category, error) {
+	args := m.Called()
+	result := args.Get(0)
+	return result.(*model.Category), args.Error(1)
+}
+
+// Mock AllCategories
+func (m *MockCategoryRepository) AllCategories(pageStr, perPageStr int) ([]*model.Category, error) {
+	args := m.Called()
+	result := args.Get(0)
+	return result.([]*model.Category), args.Error(1)
+}
+
+// Mock CategoryByID
+func (m *MockCategoryRepository) CategoryByID(id int64) (*model.Category, error) {
+	args := m.Called()
+	result := args.Get(0)
+	return result.(*model.Category), args.Error(1)
+}
+
+// Mock UpdateCategory
+func (m *MockCategoryRepository) UpdateCategory(category *model.Category) (*model.Category, error) {
+	args := m.Called()
+	result := args.Get(0)
+	return result.(*model.Category), args.Error(1)
+}
+
+// Mock DeleteCategory
+func (m *MockCategoryRepository) DeleteCategory(categoryId int64) error {
+	args := m.Called()
+	return args.Error(0)
+}
+
+func Test_DeleteCategory(t *testing.T) {
+	categoryID := int64(1)
+	mockRepo := new(MockCategoryRepository)
+
+	// Setup expectations
+	mockRepo.On("DeleteCategory").Return(nil)
+	testCategoryService := NewCategoryService(mockRepo)
+	err := testCategoryService.DeleteCategory(categoryID)
+
+	// Mock Assertion
+	mockRepo.AssertExpectations(t)
+
+	// Error Assertion
+	assert.Nil(t, err)
+}
+
+func Test_UpdateCategory(t *testing.T) {
+	mockRepo := new(MockCategoryRepository)
+	category := &model.Category{
+		ID:   1,
+		Name: "Updated Category",
+	}
+
+	// Setup expectations
+	mockRepo.On("UpdateCategory").Return(category, nil)
+	testCategoryService := NewCategoryService(mockRepo)
+	updatedCategory, err := testCategoryService.UpdateCategory(category)
+
+	// Mock Assertion
+	mockRepo.AssertExpectations(t)
+
+	// Data Assertion
+	assert.Equal(t, category.ID, updatedCategory.ID)
+	assert.Equal(t, category.Name, updatedCategory.Name)
+	assert.Nil(t, err)
+}
+
+func Test_CreateCategory(t *testing.T) {
+	var identifier int64 = 1
+	mockRepo := new(MockCategoryRepository)
+
+	category := &model.Category{
+		ID:   identifier,
+		Name: "test",
+	}
+
+	// Setup expectations
+	mockRepo.On("CreateCategory").Return(category, nil)
+	testCategoryService := NewCategoryService(mockRepo)
+	result, err := testCategoryService.CreateCategory(category)
+
+	// Mock Assertion
+	mockRepo.AssertExpectations(t)
+
+	// Data Assertion
+	assert.Equal(t, identifier, result.ID)
+	assert.Equal(t, "test", result.Name)
+	assert.Nil(t, err)
+}
+
+func Test_CategoryByID(t *testing.T) {
+	var identifier int64 = 1
+	mockRepo := new(MockCategoryRepository)
+
+	category := &model.Category{
+		ID:   identifier,
+		Name: "test",
+	}
+
+	// Setup expectations
+	mockRepo.On("CategoryByID").Return(category, nil)
+	testCategoryService := NewCategoryService(mockRepo)
+	result, err := testCategoryService.CategoryByID(identifier)
+
+	// Mock Assertion
+	mockRepo.AssertExpectations(t)
+
+	// Data Assertion
+	assert.Equal(t, identifier, result.ID)
+	assert.Equal(t, "test", result.Name)
+	assert.Nil(t, err)
+}
+
+func Test_AllCategory(t *testing.T) {
+	var identifier int64 = 1
+	mockRepo := new(MockCategoryRepository)
+	category := &model.Category{
+		ID:   identifier,
+		Name: "test",
+	}
+	// Setup expectations
+	mockRepo.On("AllCategories").Return([]*model.Category{category}, nil)
+	testCategoryService := NewCategoryService(mockRepo)
+	result, err := testCategoryService.AllCategory("1", "2")
+
+	// Mock Assertion
+	mockRepo.AssertExpectations(t)
+
+	// Data Assertion
+	assert.Equal(t, identifier, result[0].ID)
+	assert.Equal(t, "test", result[0].Name)
+	assert.Nil(t, err)
+
+}
+
+func Test_CreateCategory_EmptyCategory(t *testing.T) {
+	service := NewCategoryService(nil)
+	data, err := service.CreateCategory(nil)
+
+	assert.NotNil(t, err)
+	assert.Nil(t, data)
+	assert.Equal(t, "the category is empty", err.Error())
+}
+
+func Test_CreateCategory_CategoryName(t *testing.T) {
+	service := NewCategoryService(nil)
+	category := &model.Category{}
+	data, err := service.CreateCategory(category)
+
+	assert.NotNil(t, err)
+	assert.Nil(t, data)
+	assert.Equal(t, "the name category is empty", err.Error())
+
+}
