0a502bf786b0a06e86c8e642938edc9c015f9ad8
armin <genius2001armin@gmail.com>
2023-07-03 17:04:18 +0330

    service_interface: return CreateCategory and UpdateCategory change it to (*cm.Category,error)

--- pkg/category/repository/category_repository_interface.go
+++ pkg/category/repository/category_repository_interface.go
@@ -7,7 +7,7 @@ import (
 type CategoryRepositoryInterface interface {
 	AllCategories(page, perPage int) ([]*cm.Category, error)
 	CategoryByID(categoryId int64) (*cm.Category, error)
-	CreateCategory(category *cm.Category) error
-	UpdateCategory(category *cm.Category) error
+	CreateCategory(category *cm.Category) (*cm.Category, error)
+	UpdateCategory(category *cm.Category) (*cm.Category, error)
 	DeleteCategory(categoryId int64) error
 }
